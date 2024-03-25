7c7e074f11a8dc9bfe1bf682ecdc32ef108227b0
Sultan Suleimenov <ss.softwareit@gmail.com>
2024-03-26 00:07:09 +0500

    trying to add auth

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,14 +23,23 @@ repositories {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-encoding:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-json:$ktor_version")
+    implementation("com.fleeksoft.ksoup:ksoup:0.1.2")
+    implementation("com.fleeksoft.ksoup:ksoup-network:0.1.2")
+    implementation("io.ktor:ktor-client-apache:1.3.2-1.4-M2")
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-host-common-jvm")
+    implementation("io.ktor:ktor-client-logging:$ktor_version")
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("io.ktor:ktor-server-auth-jvm")
     implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-client-okhttp-jvm:2.3.8")
+    implementation("io.ktor:ktor-client-cio-jvm:2.3.8")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
--- src/main/kotlin/com/softwareit/plugins/Routing.kt
+++ src/main/kotlin/com/softwareit/plugins/Routing.kt
@@ -1,22 +1,41 @@
 package com.softwareit.plugins
 
 import com.softwareit.routes.students
+import io.ktor.client.*
+import io.ktor.client.engine.apache.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.util.*
+import kotlinx.coroutines.runBlocking
 
+
+@OptIn(InternalAPI::class)
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
         }
+    }
 
-//        // Static plugin. Try to access `/static/index.html`
-//        static("/static") {
-//            resources("static")
-//        }
-        randomStudent()
+    routing {
+        get("/v1/student") {
+            call.respond(
+                HttpStatusCode.OK,
+                students.random(),
+            )
+        }
+        post("/v1/student") {
+            call.respond(
+                HttpStatusCode.Accepted,
+                students.random(),
+            )
+        }
     }
     routing {
         get("/v1/students") {
@@ -25,21 +44,82 @@ fun Application.configureRouting() {
                 students
             )
         }
+    }
 
+    val credentials = Credentials(
+        username = "200107106",
+        password = "asdfgh890",
+    )
+
+    val urr = "https://ktor.io/docs/request.html#binary"
+    val exampleUrl = "https://amedia.site/1542-monolog-farmacevta.html"
+
+
+    routing {
+        get("/v2/auth") {
+            val client = HttpClient(CIO)
+            val response = client.get(exampleUrl)
+            call.respondText(response.bodyAsText())
+            client.close()
+        }
+        post("/v2/auth") {
+            val client = HttpClient(CIO)
+            val response = client.get(exampleUrl)
+            call.respondText(response.bodyAsText())
+            client.close()
+        }
+    }
+
+    val targetUrl = "https://oldmy.sdu.edu.kz/loginAuth.php"
+
+    val myFormData = FormDataContent(Parameters.build {
+        append("username", "200107106")
+        append("password", "asdfgh890")
+        append("modstring", "")
+        append("LogIn", " Log in ")
+    })
+
+    routing {
+        get("/v1/auth") {
+            val client = HttpClient(Apache) {
+                followRedirects = true
+            }
+
+            runBlocking {
+                val response: HttpResponse = client.post("https://oldmy.sdu.edu.kz/loginAuth.php") {
+                    body = FormDataContent(
+                        Parameters.build {
+                            append("username", "200107106")
+                            append("password", "asdfgh890")
+                            append("modstring", "")
+                            append("LogIn", " Log in ")
+                        }
+                    )
+                    headers {
+                        append(HttpHeaders.ContentType, ContentType.MultiPart.FormData)
+                        append(HttpHeaders.Cookie, "PHPSESSID=ssbu49jagvpfl6tn7t426reh37; uname=200107106")
+                    }
+                }
+
+                println("Response status: ${response.status}")
+
+                if (response.status.isSuccess()) {
+                    // Get HTML content from the redirected response
+                    val htmlContent = response.bodyAsText()
+                    println("HTML content: $htmlContent")
+                } else {
+                    println("Error: ${response.status}")
+                }
+                call.respondText(response.bodyAsText())
+            }
+
+
+            client.close()
+        }
     }
 }
 
-//fun Application.registerRandomStudentRoutes() {
-//    routing {
-//        randomStudent()
-//    }
-//}
-
-fun Route.randomStudent() {
-    get("/v1/student") {
-        call.respond(
-            HttpStatusCode.OK,
-            students.random(),
-        )
-    }
-}
+data class Credentials(
+    val username: String,
+    val password: String
+)
